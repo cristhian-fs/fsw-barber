b186bdffdb7d8328899e91e4f1372bb525e7ffdb
Cristhian <cristhianfernandolp@gmail.com>
2024-08-08 20:03:52 -0300

    feat: add initial barbershop page

---
+++ src/app/barbershop/[id]/page.tsx
@@ -0,0 +1,71 @@
+import { db } from "@/app/_lib/prisma"
+import Image from "next/image"
+import { Button } from "@/app/_components/ui/button"
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
+import Link from "next/link"
+import { notFound } from "next/navigation"
+
+interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+
+  if (!barbershop) {
+    return notFound()
+  }
+  return (
+    <div>
+      {/* IMAGEM */}
+      <div className="relative h-[250px] w-full">
+        <Image
+          src={barbershop?.imageUrl}
+          alt={barbershop?.name}
+          fill
+          className="object-cover"
+        />
+        <Button
+          size="icon"
+          variant={"secondary"}
+          className="absolute left-2 top-2"
+          asChild
+        >
+          <Link href={"/"}>
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+        <Button
+          size="icon"
+          variant={"secondary"}
+          className="absolute right-2 top-2"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
+        <div className="flex items-center gap-2">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop?.address}</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <StarIcon className="fill-primary stroke-none" size={18} />
+          <p className="text-sm">5,0 (499 avaliações)</p>
+        </div>
+      </div>
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
+        <p className="text-sm">{barbershop?.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopPage
